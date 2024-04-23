139531f8515ea8fb0d0696879f1881875ed41957
vinayak <vinayakvasudevgit@gmail.com>
2024-04-23 03:18:46 -0700

    version4

--- src/components/Accodion.js
+++ src/components/Accodion.js
@@ -4,45 +4,64 @@ import './Accordion.css'
 
 const Accodion = () => {
 
-const arr=[]
-
 const [item,setItem]=useState(null)
+const [multiselection,setmultiselection]=useState(false)
 const [store,setstore]=useState([])
-
-
 
 const singleselection=(recieveid)=>{
   setItem(
-    recieveid === item ?
-      null 
+    recieveid === item 
+    ? null 
     : recieveid
   );
 
-  setstore(recieveid)
 }
+
+const enablemultiselection=(recieveid)=>{
+
+ const idstore=[...store]
+ const findindex=idstore.indexOf(recieveid)
+
+    if(findindex===-1){
+      idstore.push(recieveid)
+    }
+    else{
+      idstore.splice(findindex,1)
+    }
+
+ setstore(idstore)
+}
+
 console.log(store)
 
-
-  
   return (
     <div className='wrapper'>
-      <button>Enable multiselection</button>
+      <button onClick={ ()=>setmultiselection(!multiselection)}>Enable multiselection</button>
       <div className='accordion'>
+
       {
         mydata && mydata.length>0 ? 
             (mydata.map((dataitems) => (
-              <div onClick={()=>singleselection(dataitems.id)} key={dataitems.id} className='div1'>
+
+              <div key={dataitems.id} className='div1'  
+                  onClick={ 
+                    multiselection 
+                    ? ()=>enablemultiselection(dataitems.id) 
+                    : ()=>singleselection(dataitems.id) 
+                    } > 
+                
                 <div className='div2'>
                   <h3>{dataitems.question}</h3>
                   <span>+</span>
                 </div>
+
                     <div className='div3'>
-                      {
-                        item===dataitems.id ?
-                        <h5>{dataitems.answer}</h5>
-                        :null
+                      { multiselection 
+                      ? store.indexOf(dataitems.id) !==-1 && <h5>{dataitems.answer}</h5>
+                      : item===dataitems.id && <h5>{dataitems.answer}</h5>
                       }
                     </div>
+
               </div>
             )))
         : (<div>no data found!</div>) 
